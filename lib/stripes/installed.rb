d4cc5355df48823f61fac3184ed99814553a44e9
Kristan Krispy Uccello <kuccello@gmail.com>
2009-09-17 16:44:06 -0400

    -- removed installed.rb

--- lib/stripes/installed.rb
+++
@@ -1,6 +0,0 @@
-# REMOVE THIS FILE TO HAVE SETUP RUN AGAIN
-puts "=============================================="
-puts " Stripes v0.1 has been setup - Sep. 17, 2009"
-puts "=============================================="
-$STRIPES_INSTALLED = true
-# PUTS SETTINGS IN PLACE FOR THIS INSTANCE OF THE APPLICATION
